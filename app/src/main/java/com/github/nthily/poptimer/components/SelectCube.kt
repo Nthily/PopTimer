e35133a382f2fc6a864133746319b79aa589e970
Nthily <nthily@outlook.com>
2021-08-06 22:59:09 +0200

    update localization

--- app/src/main/java/com/github/nthily/poptimer/components/SelectCube.kt
+++ app/src/main/java/com/github/nthily/poptimer/components/SelectCube.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -71,7 +72,7 @@ fun SelectCubeSheetContent() {
             .padding(8.dp)
     ) {
         Text(
-            text = "选择一种魔方",
+            text = stringResource(id = R.string.select_cube),
             fontWeight = FontWeight.W700
         )
         Button(onClick = { appViewModel.selectCube = false }) {
